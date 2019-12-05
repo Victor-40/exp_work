9887d17f6e071e1d882d9a886466b84c6f7fa34c
Victor Cherezov <victor_cherezov@mentor.com>
2019-12-05 18:55:35 +0300

    wip

--- flask_rest/api.py
+++ flask_rest/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
 import json
+import time
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,9 +15,9 @@ cors = CORS(app)
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('key1')
-parser.add_argument('key2')
-parser.add_argument('key3')
+parser.add_argument('path')
+parser.add_argument('vm')
+parser.add_argument('snapshot')
 
 
 class SingleTask(Resource):
@@ -26,6 +27,7 @@ class SingleTask(Resource):
     def post(self):
         args = parser.parse_args()
         print(args)
+        time.sleep(20)
         return args
 
 
